6abdae40a3ac07b6dd637ea46654385b0c6ece34
ANAS-BAKI-SENHAJI <anasssenhaji8484@gmail.com>
2021-05-29 13:31:09 +0100

    first commit

---
+++ app/Models/Activity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Activity extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'price', 
+        'units', 
+        'description', 
+        'images', //should contain several images (Idk if it's possible)
+        'activity_date',
+        'activity_time',
+        'duration_of_activity',
+        'min_number_of_people',
+        'max_number_of_people',
+        'average_stars',
+        'included_equipment', //boolean
+        'included_transport', //boolean
+        'equipments_included' //will contain many equipments
+    ];
+
+    public function reservations(){
+        return $this->hasMany(Reservation::class);
+    }
+}
