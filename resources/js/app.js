89ed271804b501d20974d719fabfd10ca543857c
ANAS-BAKI-SENHAJI <anasssenhaji8484@gmail.com>
2021-05-31 14:43:58 +0100

    added the Login, register, UserBoard, admin vues and Main, Users Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,100 @@
-require('./bootstrap');
+import Vue from 'vue'
+    import VueRouter from 'vue-router'
+
+    Vue.use(VueRouter)
+
+    //import App from './App'
+    //import Home from './Home'
+    import Login from './Login'
+    import Register from './Register'
+    //import SingleProduct from './SingleProduct'
+    //import Checkout from './Checkout'
+    //import Confirmation from './Confirmation'
+    import UserBoard from './UserBoard'
+    import Admin from './Admin'
+
+    const router = new VueRouter({
+        mode: 'history',
+        routes: [
+            {
+                path: '/',
+                name: 'home',
+                component: Home
+            },
+            {
+                path: '/login',
+                name: 'login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'register',
+                component: Register
+            },
+            {
+                path: '/dashboard',
+                name: 'userboard',
+                component: UserBoard,
+                meta: {
+                    requiresAuth: true,
+                    is_user: true
+                }
+            },
+            {
+                path: '/admin/:page',
+                name: 'admin-pages',
+                component: Admin,
+                meta: {
+                    requiresAuth: true,
+                    is_admin: true
+                }
+            },
+            {
+                path: '/admin',
+                name: 'admin',
+                component: Admin,
+                meta: {
+                    requiresAuth: true,
+                    is_admin: true
+                }
+            },
+        ],
+    })
+
+    router.beforeEach((to, from, next) => {
+        if (to.matched.some(record => record.meta.requiresAuth)) {
+            if (localStorage.getItem('bigStore.jwt') == null) {
+                next({
+                    path: '/login',
+                    params: { nextUrl: to.fullPath }
+                })
+            } else {
+                let user = JSON.parse(localStorage.getItem('bigStore.user'))
+                if (to.matched.some(record => record.meta.is_admin)) {
+                    if (user.is_admin == 1) {
+                        next()
+                    }
+                    else {
+                        next({ name: 'userboard' })
+                    }
+                }
+                else if (to.matched.some(record => record.meta.is_user)) {
+                    if (user.is_admin == 0) {
+                        next()
+                    }
+                    else {
+                        next({ name: 'admin' })
+                    }
+                }
+                next()
+            }
+        } else {
+            next()
+        }
+    })
+
+    const app = new Vue({
+        el: '#app',
+        components: { App },
+        router,
+    });
